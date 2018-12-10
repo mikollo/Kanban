7a98b7550903d86084b2db5c60df9fa58bf89081
Mikołaj Nowakowski <mikolajkrzysztofnowakowski@gmail.com>
2018-12-10 02:00:50 +0100

    Add test

---
+++ src/__tests__/list.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { render, fireEvent, cleanup } from "react-testing-library";
+// this adds custom jest matchers from jest-dom
+import "jest-dom/extend-expect";
+import Kanban from "../Kanban";
+
+// automatically unmount and cleanup DOM after the test is finished.
+afterEach(cleanup);
+
+test("List can be added", async () => {
+  const { getByText, getByPlaceholderText } = render(<Kanban />);
+  const input = getByPlaceholderText("New list name");
+  (input as any).value = "Some list";
+  fireEvent.change(input);
+  fireEvent.click(getByText("Add new list"));
+  const createdListHeader = getByText("Add new task");
+  expect(createdListHeader).toBeDefined();
+});
